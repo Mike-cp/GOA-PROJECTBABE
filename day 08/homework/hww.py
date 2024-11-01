373b1e5913d5becb0a64b8e99e08133d7c558658
Mike-cp <Mnomore99@gmail.com>
2024-11-02 01:41:45 +0400

    commit

---
+++ day 08/homework/hww.py
@@ -0,0 +1,16 @@
+num1=10
+num2=20
+num3=30
+num4=40
+
+print((num1 + num2) - num3 +num3 / num1 * num2)
+print((num3 / 10) - num1 + num4 * num4)
+print((num1 * num2) + (num3 - num4))               
+print(num4 / num1 + num3 - num2 * num1)             
+print(num2 * num4 - num3 + num1 / num2)             
+print((num4 - num1) * (num3 + num2) / num1)         
+print(num1 + num2 + num3 + num4 / num1)             
+print(num3 - num1 * num4 + num2 / num3)             
+print(num4 + num2 - num3 * num1 / num2)             
+print((num1 + num4) * (num2 - num3) / num4)         
+print((num1 + num2) * (num3 + num4) - num4 / num1)
---
+++ day 08/homework/ჰწწ.py
@@ -0,0 +1,4 @@
+name = input("yourname:")
+print("name:",name)
+surname = input("surname:")
+print("name",name)
