9457ec05e75f5eadbe69f607313fe6d854ac3070
Mike-cp <Mnomore99@gmail.com>
2024-11-02 00:56:25 +0400

    Initial commit

---
+++ day 08/classwork/hw.py
@@ -0,0 +1,11 @@
+name = input(" name: ")
+surname = input(" surname: ")
+age = input("age: " )
+number1 = input("number2: " )
+number2 = input("number2: " )
+
+print(type(name))
+print(type(surname))
+print(type(age))
+print(type(number1))
+print(type(number2))
